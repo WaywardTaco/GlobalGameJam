32f02fabd8366ea642694313ccc48848021c661e
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2025-01-24 20:09:49 +0800

    updates

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Resource : MonoBehaviour
+public class ResourceManager : MonoBehaviour
 {
 
-    public static Resource Instance;
+    public static ResourceManager Instance;
 
     void Awake()
     {
