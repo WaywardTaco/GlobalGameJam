437eb5a3a29acb77188d4a844c4c166e23738e39
DevNikz <paul_ragudo@dlsu.edu.ph>
2025-01-25 17:09:46 +0800

    added upgrades to main scene

--- Assets/Scripts/Systems/StartupManager.cs
+++ Assets/Scripts/Systems/StartupManager.cs
@@ -9,6 +9,10 @@ public class StartupManager : MonoBehaviour
     private GameObject newsGUI;
     private GameObject stocksGUI;
     private GameObject upgradeGUI;
+    private float time = 1.5f;
+    private float currentTime;
+    public bool loadScreen;
+    public bool hasLoaded;
     void Awake() {
         //Login Screen
         LoginGUI = GameObject.Find("LoginGUI").gameObject;
@@ -16,11 +20,38 @@ public class StartupManager : MonoBehaviour
         //Window Tabs
         windowGUI = GameObject.Find("WindowGUI").gameObject;
         newsGUI = GameObject.Find("NewsfeedUI").gameObject;
+        upgradeGUI = GameObject.Find("Upgrade Menu").gameObject;
+
+        currentTime = time;
     }
 
     void Start() {
         LoginGUI.SetActive(true);
         windowGUI.SetActive(false);
         newsGUI.SetActive(false);
+        upgradeGUI.SetActive(false);
     }
+
+    void Update() {
+        if(loadScreen) {
+            currentTime -= Time.deltaTime;
+            if(currentTime <= 0) {
+                loadScreen = false;
+                hasLoaded = true;
+            }
+        }
+        if(hasLoaded) {
+            LoginGUI.SetActive(false);
+            windowGUI.SetActive(true);
+        }
+    }
+
+    public void SetLoadScreen(bool value) {
+        loadScreen = value;
+    }
+    
+    public void SetHasLoaded(bool value) {
+        hasLoaded = value;
+    }
+
 }
