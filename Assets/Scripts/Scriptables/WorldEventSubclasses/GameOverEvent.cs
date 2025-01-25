b29731fe10421ddbf3033454e732cfd2746fecac
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2025-01-25 20:36:10 +0800

    events

--- Assets/Scripts/Scriptables/WorldEventSubclasses/GameOverEvent.cs
+++ Assets/Scripts/Scriptables/WorldEventSubclasses/GameOverEvent.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Enums;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "New WorldEvent", menuName = "Scriptables/WorldEvents/GameOverEvent", order = 1)]
 public class GameOverEvent : ReputationDetector
 {
+    private GameOverScript gameOverScript;
+
     public override void OnActivate()
     {
-        // TODO : Call game over text
+        gameOverScript = GameObject.Find("Canvas")?.GetComponent<GameOverScript>();
+        if(gameOverScript != null){
+            gameOverScript.hasDied = false;
+            SceneManager.LoadScene("Game Over");
+        }
     }
 
 }
