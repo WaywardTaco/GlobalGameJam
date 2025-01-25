ad92d5a97398c79632c737b908984dc7e66ee405
Angelo Gabriel <ajworks2004@gmail.com>
2025-01-25 11:48:31 +0800

    Fixed Error

--- Assets/Scripts/Managers/ScoreboardManager.cs
+++ Assets/Scripts/Managers/ScoreboardManager.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class UserScore : MonoBehaviour
 {
 
-    private string Name;
-    private int Money;
-    private int Days;
+    public string Name;
+    public int Money;
+    public int Days;
+
+    public UserScore(string name, int money, int days)
+    {
+        Name = name;
+        Money = money;
+        Days = days;
+    }
 
     public string GetName() { return Name; }
     public int GetMoney() { return Money; }
@@ -18,14 +26,14 @@ public class UserScore : MonoBehaviour
 public class ScoreboardManager : MonoBehaviour
 {
 
-    private UserScore[] ScoreBoard;
+    static UserScore[] ScoreBoard;
     Dictionary<UserScore, int> Ranks = new Dictionary<UserScore, int>();
-    IEnumerable<UserScore> UserRankings = UserScore.OrderBy(UserScore => UserScore.Days);
+    IEnumerable<UserScore> UserRankings = ScoreBoard.OrderBy(UserScore => UserScore.Days);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
