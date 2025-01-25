1f1576f2bb7d9094a333e1b0071126da8b8d346b
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2025-01-25 18:15:27 +0800

    thing

---
+++ Assets/Scripts/Scriptables/WorldEventSubclasses/StockAffector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enums;
+
+[CreateAssetMenu(fileName = "New WorldEvent", menuName = "Scriptables/WorldEvents/StockAffector", order = 1)]
+public class StockAffector : GameWorldEventScriptable
+{
+    public override void OnActivate(){
+
+    }
+    
+    public override void OnDeactivate(){
+
+    }
+    
+    public override void ContinuousEffect(){
+
+    }
+    
+}
