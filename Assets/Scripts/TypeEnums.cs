af8f0674345ddf17c30dcc49e6ea42600493e5d8
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2025-01-26 09:11:39 +0800

    yes

--- Assets/Scripts/TypeEnums.cs
+++ Assets/Scripts/TypeEnums.cs
@@ -10,14 +10,10 @@ namespace Enums {
         LMEMeme, PPHMeme, BIDMeme, OBGMeme, SFIMeme, LDDMeme,
         LMEGovAllow, PPHGovAllow, BIDGovAllow, OBGGovAllow, SFIGovAllow, LDDGovAllow,
         LMEGovBan, PPHGovBan, BIDGovBan, OBGGovBan, SFIGovBan, LDDGovBan,
-        LMEVirus, PPHVirus, BIDVirus, OBGVirus, SFIVirus, LDDVirus,
-<<<<<<< HEAD
-        PolInv0, PolInv1, PolInv2, PolInv3, PolInv4, PolInv5 , CSR, Tax, 
+        LMEVirus, PPHVirus, BIDVirus, OBGVirus, SFIVirus, LDDVirus, CSR, Tax, 
         Charity, Diversity, Lobbying, Insider, Pyramid, Blackmail, Shell, 
-        Downpayment, Hacker, Bond, PR, Bitcoin, Narrative
-=======
+        Downpayment, Hacker, Bond, PR, Bitcoin, Narrative,
         PolInv0, PolInv1, PolInv2, PolInv3, PolInv4, PolInv5, PolInv6,
->>>>>>> 10b09c1fb92156602dcc4c34dee6a401d35e2d89
     }
     
     public enum UpgradeType {
